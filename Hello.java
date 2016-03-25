f4a38b9f8f9c73579303bd2189210b3d5d29752e
PheonixHkbxoic <hkbxoic@gmail.com>
2016-03-25 15:57:34 +0800

    1
    第一次修改，并提交

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public  class Hello{
+
+	public void print(){
+		System.out.println("I am Programmer");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Pheonix !");
+		new Hello().print();
+	}
+}
